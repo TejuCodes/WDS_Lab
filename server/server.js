1d06ec484ea0943c7fa794c29f68c2c049e78e1f
Teju <tejashwin2004teju@gmail.com>
2026-08-01 11:36:16 +0530

    Created an Server Page

--- server/server.js
+++ server/server.js
@@ -6,12 +6,11 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 
-// Connect Database
-connectDB();
+connectDB(); //DataBase Start Panna 
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
@@ -25,5 +24,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Nanba Server Running la iruku `);
 });
