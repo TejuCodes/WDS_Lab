c95fdeadb152e3974c37658b8f1a7503f876d69e
Teju <tejashwin2004teju@gmail.com>
2026-08-01 11:34:31 +0530

    Created an Config File for Database

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "🚀 SecureLearn Backend Running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
